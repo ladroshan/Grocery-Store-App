33724ca7aa727a54bcc3bc42a77ae25d9fd7080c
shazaman23 <intechninja@gmail.com>
2016-10-01 13:04:56 -0600

    Created the login pages. **Test code still exists **JDBC Needs to be created and added.

--- src/main/Mainframe.java
+++ src/main/Mainframe.java
@@ -3,38 +3,182 @@ package main;
 import main.MenuBar;
 
 import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 
 /**
  * @author Jacob Killpack
  *
  */
-public class Mainframe{
+public class Mainframe implements ActionListener{
 	
+	//This is the JFrame that runs the program.
 	private JFrame frame =  new JFrame("ScanableGrocery");
-	private Container pane;
-	MenuBar menu = new MenuBar();
 	
+	//This is the pane in the JFrame
+	private Container pane;
+	
+	//This is the instance of the Menubar being created. The menu object is being initialized with the 
+	//overloaded constructor to make the Menubar editable later in this program.
+	//The parameter being passed is this Mainframe instance.
+	MenuBar menu = new MenuBar(this);
+	
+	//NONUSER is just someone who is not logged in.
+	private enum userType { NONUSER, CASHIER, ADMIN };
+	
+	//This variable is to track which form of user is using the application
+	private userType current;
+	
+	//This is the login button
+	private JButton loginBtn, submit;
+	
+	//REMOVE ME!! label is a test JLabel
+	private JLabel label;
+	
+	/**
+	 * Initialization constructor for Mainframe
+	 * 
+	 */
 	public Mainframe(){
+		current = userType.ADMIN;
+		checkUser();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pane = frame.getContentPane();
-		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+		pane.setLayout(new GridBagLayout());
 		frame.setJMenuBar(menu);
 		frame.pack();
 		frame.setSize(1000, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
-
+	
+	private void reload(){
+		checkUser();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pane = frame.getContentPane();
+		pane.setLayout(new GridBagLayout());
+		frame.setJMenuBar(menu);
+		frame.pack();
+		frame.setSize(1000, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
+	private void paneEdit(String page, String section) {
+		if(page == "login") {
+			if (section == "button") {
+				loginBtn = new JButton("Login");
+				loginBtn.addActionListener(this);
+				pane.add(loginBtn);
+			}
+			else if (section == "form"){
+				JPanel loginForm = new JPanel();
+				loginForm.setLayout(new GridBagLayout());
+				GridBagConstraints design = new GridBagConstraints();
+				design.insets = new Insets(15, 15, 15, 15);
+				JLabel luser = new JLabel("Username: ");
+				JLabel lpass = new JLabel("Password: ");
+				JTextField uname = new JTextField(20);
+				JPasswordField pword = new JPasswordField(20);
+				submit = new JButton("Submit");
+				submit.addActionListener(this);
+				design.gridx = 0;
+				design.gridy = 0;
+				loginForm.add(luser, design);
+				design.gridx = 0;
+				design.gridy = 1;
+				loginForm.add(lpass, design);
+				design.gridx = 2;
+				design.gridy = 0;
+				loginForm.add(uname, design);
+				design.gridx = 2;
+				design.gridy = 1;
+				loginForm.add(pword, design);
+				design.gridx = 1;
+				design.gridy = 2;
+				loginForm.add(submit, design);
+				pane.add(loginForm);
+			}
+		}
+		//REMOVE ME!! This else statement was used for testing only. 
+		else if (page == "cashier") {
+			label = new JLabel("This is the Cashier Page");
+			pane.add(label);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "There was an error in the paneEdit"
+					+ " function. Please review the page that is being passed.", "THERE WAS AN ERROR!", 
+					JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	protected void logout() {
+		//set user to 'logged out' status
+		current = userType.NONUSER;
+		pane.removeAll();
+		frame.dispose();
+		paneEdit("login", "button");
+		reload();
+	}
+	
+	private void checkUser() {
+		if (current == userType.NONUSER){
+			menu.hideMenu("pages", "all");
+			menu.hideMenu("file", "logout");
+		}
+		else if (current == userType.CASHIER) {
+			menu.hideMenu("pages", "inventory");
+			menu.hideMenu("pages", "users");
+		}
+		else if (current == userType.ADMIN) {
+			//Don't hide any options from the ADMIN
+		}
+		else {
+			//NULL
+		}
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new Mainframe();
+		//Mainframe test = new Mainframe();
+		//test.logout();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == loginBtn) {
+			pane.removeAll();
+			frame.dispose();
+			paneEdit("login", "form");
+			reload();
+		}
+		if (e.getSource() == submit) {
+			
+		}
+	}
+	/**
+	 * REMOVE ME!! This method was used for testing only.
+	 */
+	protected void cash() {
+		current = userType.CASHIER;
+		pane.removeAll();
+		frame.dispose();
+		paneEdit("cashier", "button");
+		reload();
 	}
 
 }
--- src/main/MenuBar.java
+++ src/main/MenuBar.java
@@ -1,6 +1,6 @@
 package main;
 
-//import main.Mainframe;
+import main.Mainframe;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,31 +10,118 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
+
 /**
  * @author Jacob Killpack
  *
  */
 public class MenuBar extends JMenuBar implements ActionListener{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1187579188283252389L;
 	JMenuBar menuBar;
-	JMenu file, about, help;
-	JMenuItem exit;
+	JMenu file, pages, about, help;
+	JMenuItem logout, cashier, exit;
+	JMenuItem inventory, users, checkout;
 	JMenuItem details;
 	JMenuItem instructions, bugReport;
+	
+	Mainframe frame;
 	
 	public MenuBar(){
 		//Add headers to the Menubar
 		file = new JMenu("File");
 		this.add(file);
+		pages = new JMenu("Pages");
+		this.add(pages);
 		about = new JMenu("About");
 		this.add(about);
 		help = new JMenu("Help");
 		this.add(help);
 		
 		//Add the items to the file menu
+		logout = new JMenuItem("Logout");
+		logout.addActionListener(this);
+		file.add(logout);
+
+		//REMOVE ME!! This was for testing
+		cashier = new JMenuItem("Cashier");
+		cashier.addActionListener(this);
+		file.add(cashier);
+		
 		exit = new JMenuItem("Exit");
 		exit.addActionListener(this);
 		file.add(exit);
+		
+		//Add the items to the pages menu
+		inventory = new JMenuItem("Inventory");
+		inventory.addActionListener(this);
+		pages.add(inventory);
+		
+		users = new JMenuItem("User Management");
+		users.addActionListener(this);
+		pages.add(users);
+		
+		checkout = new JMenuItem("Checkout");
+		checkout.addActionListener(this);
+		pages.add(checkout);
+		
+		//Add the items to the about menu
+		details = new JMenuItem("About the Program");
+		details.addActionListener(this);
+		about.add(details);
+		
+		//Add the items to the help menu
+		instructions = new JMenuItem("Instructions");
+		instructions.addActionListener(this);
+		help.add(instructions);
+		
+		bugReport = new JMenuItem("Report a Bug");
+		bugReport.addActionListener(this);
+		help.add(bugReport);
+	}
+	
+	public MenuBar(Mainframe mainframe){
+		frame = mainframe;
+		
+		//Add headers to the Menubar
+		file = new JMenu("File");
+		this.add(file);
+		pages = new JMenu("Pages");
+		this.add(pages);
+		about = new JMenu("About");
+		this.add(about);
+		help = new JMenu("Help");
+		this.add(help);
+		
+		//Add the items to the file menu
+		logout = new JMenuItem("Logout");
+		logout.addActionListener(this);
+		file.add(logout);
+		
+		//REMOVE ME!! This was for testing
+		cashier = new JMenuItem("Cashier");
+		cashier.addActionListener(this);
+		file.add(cashier);
+		
+		exit = new JMenuItem("Exit");
+		exit.addActionListener(this);
+		file.add(exit);
+		
+		//Add the items to the pages menu
+		inventory = new JMenuItem("Inventory");
+		inventory.addActionListener(this);
+		pages.add(inventory);
+		
+		users = new JMenuItem("User Management");
+		users.addActionListener(this);
+		pages.add(users);
+		
+		checkout = new JMenuItem("Checkout");
+		checkout.addActionListener(this);
+		pages.add(checkout);
 		
 		//Add the items to the about menu
 		details = new JMenuItem("About the Program");
@@ -57,6 +144,33 @@ public class MenuBar extends JMenuBar implements ActionListener{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MenuBar();
+	}
+	
+	public void hideMenu(String hide, String subs) {
+		
+		if (hide == "pages") {
+			if (subs == "inventory"){
+				pages.remove(inventory);
+			}
+			else if (subs == "users"){
+				pages.remove(users);
+			}
+			else if (subs == "checkout"){
+				pages.remove(checkout);
+			}
+			else {
+				this.remove(pages);
+			}
+		}
+		
+		if (hide == "file") {
+			if (subs == "logout") {
+				file.remove(logout);
+			}
+			else {
+				this.remove(file);
+			}
+		}
 	}
 
 	@Override
@@ -84,6 +198,13 @@ public class MenuBar extends JMenuBar implements ActionListener{
 					+ "Include in the email body a description of the error you are "
 					+ "experiencing.", "Instructions", 
 					JOptionPane.INFORMATION_MESSAGE);
+		}
+		if (e.getSource() == logout) {
+			frame.logout();
+		}
+		//REMOVE ME!! This was for testing
+		if (e.getSource() == cashier) {
+			frame.cash();
 		}
 	}
 
